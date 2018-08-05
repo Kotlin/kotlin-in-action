4e5c8865faa94852e46b929643c74393bcaa7877
Hiroshi Kouta <hiroshikouta@hiroshinoMacBook-puro.local>
2018-08-05 14:46:52 +0900

    Changed Embironment IntelliJ

--- src/ch07/7.2.1_EqualityOperators.kt
+++ src/ch07/7.2.1_EqualityOperators.kt
@@ -1,10 +1,10 @@
 package ch07.ex2_1_EqualityOperators
 
 class Point(val x: Int, val y: Int) {
-    override fun equals(obj: Any?): Boolean {
-        if (obj === this) return true
-        if (obj !is Point) return false
-        return obj.x == x && obj.y == y
+    override operator fun equals(other: Any?): Boolean {
+        if (other === this) return true
+        if (other !is Point) return false
+        return other.x == x && other.y == y
     }
 }
 
